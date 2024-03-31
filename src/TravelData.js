fc7880465ed6960374a508e7f984bd359346b8c4
Rumana Yeasmin <ryeasmin@oakland.edu>
2024-03-31 15:54:15 -0400

    2nd commit

---
+++ src/TravelData.js
@@ -0,0 +1,46 @@
+// It is assumed that this file will be placed inside the src folder
+// Do not forget to first import the full path of all your images that is present in the Assets sub-directory as shown below
+// import Place1Image1 from "./Assets/filename.extension" and similarly for the rest.
+// You can use the images that I have provided or use your own
+// I have provided a lorem ipsum content for description. Just use that. No need to spend time figuring out a description.
+import img5 from "./Assets/img5.jpg";
+import img6 from "./Assets/img6.jpg";
+import img7 from "./Assets/img7.jpg";
+import img8 from "./Assets/img9.jpg";
+import img9 from "./Assets/img9.jpg";
+import img10 from "./Assets/img10.jpg";
+import img2 from "./Assets/img2.jpg";
+import img3 from "./Assets/img3.jpg";
+import img4 from "./Assets/img4.jpg";
+
+const TravelData = [
+  {
+    id: 1,
+    placeHeading: "Dubai",
+    placeImg1: img2,
+    placeImg2: img3,
+    placeImg3: img4,
+    placeDescription:
+      "Dubia Roaches, Reptile Feeders — Dubia Roaches Provide 7 Times the Nutritional Benefits of Crickets. Easy to Care For. Fast. #1 Supplier Of Dubia Roaches. Dubia Roaches For Sale Online. Fast Shipping, Great Service. Dubiaroaches. Dubai Bugs. Dubai Roaches. Extra Fast Shipping. Dubia Roaches Com.",
+  },
+  {
+    id: 2,
+    placeHeading: "Grecce",
+    placeImg1: img5,
+    placeImg2: img6,
+    placeImg3: img7,
+    placeDescription:
+      "Greece is a country in southeastern Europe with thousands of islands throughout the Aegean and Ionian seas. Influential in ancient times, it's often called the cradle of Western civilization. Athens, its capital, retains landmarks including the 5th-century B.C. Acropolis citadel with the Parthenon temple. Greece is also known for its beaches, from the black sands of Santorini to the party resorts of Mykonos",
+  },
+  {
+    id: 3,
+    placeHeading: "Paris",
+    placeImg1: img8,
+    placeImg2: img9,
+    placeImg3: img10,
+    placeDescription:
+      "Paris, France's capital, is a major European city and a global center for art, fashion, gastronomy and culture. Its 19th-century cityscape is crisscrossed by wide boulevards and the River Seine. Beyond such landmarks as the Eiffel Tower and the 12th-century, Gothic Notre-Dame cathedral, the city is known for its cafe culture and designer boutiques along the Rue du Faubourg Saint-Honoré",
+  },
+];
+
+export default TravelData;
---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './hero.css';
+
+const Hero = () => {
+  return (
+    <div className="hero-container">
+      <div className="hero-content">
+        <h1>Lets start the Journey</h1>
+        <h2>My Favorite Country to visit...</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
